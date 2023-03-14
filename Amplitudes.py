a4b7cf68d4c1fa8c41f3c33b89ab0e1d3df34189
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2023-03-14 15:34:36 -0500

    comienzo

--- Amplitudes.py
+++ Amplitudes.py
@@ -1,1 +1,12 @@
 # Juan Pablo Nieto Cortes
+import libreria_complejos as lc
+import Vectores_matrices as vc
+import math
+v1 = [[-3,-1],[0,-2],[0,1],[2,0]]
+def amplitudes_complejas (v1):
+    resultado = vc.normaV(v1)
+    print(v1)
+    return resultado
+
+if __name__ == '__main__':
+    amplitudes_complejas(v1)
