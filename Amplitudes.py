b7eb41c92f003ab63d17ce0f1203c513c45e8904
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2023-03-22 12:29:48 -0500

    ok

--- Amplitudes.py
+++ Amplitudes.py
@@ -3,14 +3,13 @@ import libreria_complejos as lc
 import Vectores_matrices as vc
 import math
 
+# SIMULE EL PRIMER SISTEMA CUÁNTICO DESCRITO EN LA SECCIÓN 4.1.
 # 1. Probabilidad de observación
 def probabilidad (v1,a):
     resultado = ((lc.modulo(v1[a][0]))**2 )/(vc.normaVector(v1)**2)
     return resultado
 
 # 2. El sistema si se le da otro vector Ket debe buscar la probabilidad de transitar del primer vector al segundo.
-v1 = [[[-2,-1]],[[0,-2]],[[0,1]],[[2,0]]]
-v2 = [[[-8,-1]],[[0,-2]],[[0,1]],[[2,0]]]
 def probabilidad_transicion(v1,v2):
     # normalizar los vectores
     vn1= vc.productoEscalar_Vector(v1,(1/vc.normaVector(v1),0))
