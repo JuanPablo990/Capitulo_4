9722a51e17bff023daffd4d928a7b3f2dc72fd5e
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2023-03-28 23:53:06 -0500

    ok

--- Amplitudes.py
+++ Amplitudes.py
@@ -27,16 +27,16 @@ def amplitud_transicion(v1,v2):
     resultado = lc.division(produc[0][0][0],((vc.normaVector(v1))*(vc.normaVector(v2))))
     return resultado
 def ParticulaPosicion(ket,X):
-    return lc.detectarParticula(ket,X)
+    return detectarParticula(ket,X)
 def transitarVectorVector(ket,ket2):
 
-    return lc.transitarVector(ket,ket2)
+    return transitarVector(ket,ket2)
 def valorEsperado(obs,ket):
     obsSobreket = lc.accion(obs, ket)
-    bra = lc.matrizConjugada(obsSobreket)
-    ket1 = lc.transpuesta([ket])
-    bra1 = lc.transpuesta(bra)
-    car = lc.multiplicacionMatrizMatriz(bra1, ket1)[0][0]
+    bra = vc.conjugado_Matriz_Vector(obsSobreket)
+    ket1 = vc.transpuesta_Matriz_Vector([ket])
+    bra1 = vc.transpuesta_Matriz_Vector(bra)
+    car = vc.multiplicacionMatrizMatriz(bra1, ket1)[0][0]
     return car
 
 # 2. Ahora con una matriz que describa un observable y un vector ket, el sistema revisa que la matriz sea hermitiana, y si lo es, calcula la media y la varianza del observable en el estado dado.
@@ -64,10 +64,10 @@ def varianza(M, v):
         return round(x[0], 1)
 
 def varianzaObservable(obs,ket):
-    nve = lc.multiplicacion(valorEsperado(obs, ket), (-1, 0))
-    mve = lc.multiplicacionEscalarMatriz(lc.matrizIdentidad(len(obs)), nve)
-    delta = lc.sumaMatrices(obs, mve)
-    deltaCuadrado = lc.multiplicacionMatrizMatriz(delta, delta)
+    nve = vc.multiplicacion(valorEsperado(obs, ket), (-1, 0))
+    mve = vc.multiplicacionEscalarMatriz(lc.matrizIdentidad(len(obs)), nve)
+    delta = vc.sumaMatrices(obs, mve)
+    deltaCuadrado = vc.multiplicacionMatrizMatriz(delta, delta)
     var = valorEsperado(deltaCuadrado, ket)
 
 
@@ -107,10 +107,10 @@ def detectarParticula(ket,X):
     return (normaComplejo**2)/(normaKet**2)
 def transitarVector(ket,ket2):
     ket1=lc.transpuesta([ket])
-    bra=lc.matrizConjugada([ket2])
-    car=lc.multiplicacionMatrizMatriz(bra,ket1)[0][0]
-    normaBra=lc.normaMatriz(bra)
-    normaket=lc.normaMatriz(ket1)
+    bra=vc.matrizConjugada([ket2])
+    car=vc.multiplicacionMatrizMatriz(bra,ket1)[0][0]
+    normaBra=vc.normaMatriz(bra)
+    normaket=vc.normaMatriz(ket1)
     car2=(normaBra*normaket,0)
     return lc.division(car,car2)
 
